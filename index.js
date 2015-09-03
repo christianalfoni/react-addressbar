596a2e37e9d68304113fb9fa043ad67b671158f0
Max White <mushishi78@gmail.com>
2015-09-03 14:26:38 +0100

    More consistent function declaration

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ module.exports = React.createClass({
     event.preventDefault();
     this.props.onChange(event.target.value);
   },
-  render() {
+  render: function () {
     return null;
   }
 });
